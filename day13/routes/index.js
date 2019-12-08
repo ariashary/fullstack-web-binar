9db53390d2839922d54c057a56021adb07a4620e
Muhammad Ashary <ariashary92@gmail.com>
2019-12-08 20:55:23 +0700

    [day13] - Add mongoose library

---
+++ day13/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+// Require controller
+var article_controller = require('../controllers/articlesControllers.js');
+var application_controller = require('../controllers/applicationController.js');
+
+// Serve static file from folder assets
+// and url /assets before find assets
+router.use('/assets', express.static('assets'));
+
+// Routes
+router.get('/', application_controller.home);
+
+router.get('/about', application_controller.about);
+
+router.get('/contact', application_controller.contact);
+
+router.get('/articles', article_controller.article_list);
+
+module.exports = router;
