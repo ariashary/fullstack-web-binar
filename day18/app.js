0f650b6e001aedbd5848a4e0e3f904267056a7d5
Muhammad Ashary <ariashary92@gmail.com>
2019-12-10 14:33:22 +0700

    [day18] - Add .env and md5

--- day18/app.js
+++ day18/app.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
-
+const md5 = require('md5')
 
 const app = express();
+
+console.log(process.env.SECRET)
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -17,9 +19,6 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
-
-const secret = "Thisisourlittlesecret"
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -38,7 +37,7 @@ app.get('/register', function(req, res) {
 app.post('/register', function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
@@ -52,7 +51,7 @@ app.post('/register', function(req, res) {
 
 app.post('/login', function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, function(err, foundUser) {
         if (err) {
@@ -64,7 +63,6 @@ app.post('/login', function(req, res) {
         }
     });
 });
-
 
 app.listen(3000, function() {
     console.log('Server started on port 3000.')
